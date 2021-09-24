9d64385ece50e873c4132dd1a995279855467306
JaavLex <alexandre.javet@hotmail.com>
2021-09-24 09:31:22 +0200

    [feat] Now can remove people from list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,51 @@
 import { Box } from '@material-ui/system'
 import PlayerCardList from 'components/PlayerCardList'
-import React from 'react'
-
-const testPList = [
-  {
-    id: 1,
-    username: 'jeanmich',
-  },
-  {
-    id: 2,
-    username: 'Xx_plao',
-  },
-  {
-    id: 3,
-    username: 'AAAAAAHHHH',
-  },
-  {
-    id: 4,
-    username: 'Melvyn',
-  },
-  {
-    id: 5,
-    username: 'Ana',
-  },
-  {
-    id: 6,
-    username: 'Alex',
-  },
-  {
-    id: 7,
-    username: 'Luca',
-  },
-]
-
-function removeUser(playerId: number) {
-  testPList.forEach((e, i) => e.id == playerId && testPList.splice(i, 1))
-}
+import React, { useState } from 'react'
 
 export default function index() {
-  //const [pList, setPList] = useState(testPList)
+  const [pList, setPList] = useState([
+    {
+      id: 1,
+      username: 'jeanmich',
+    },
+    {
+      id: 2,
+      username: 'Xx_plao',
+    },
+    {
+      id: 3,
+      username: 'AAAAAAHHHH',
+    },
+    {
+      id: 4,
+      username: 'Melvyn',
+    },
+    {
+      id: 5,
+      username: 'Ana',
+    },
+    {
+      id: 6,
+      username: 'Alex',
+    },
+    {
+      id: 7,
+      username: 'Luca',
+    },
+  ])
+
+  function removeUser(playerId: number) {
+    const tempPList = [...pList]
+    pList.forEach(
+      (e: any, i: any) => e.id == playerId && tempPList.splice(i, 1)
+    )
+    setPList(tempPList)
+  }
+
   return (
     <Box display="flex" justifyContent="center">
       <PlayerCardList
-        players={testPList}
+        players={pList}
         onRemove={(id) => removeUser(id)}
       ></PlayerCardList>
     </Box>
